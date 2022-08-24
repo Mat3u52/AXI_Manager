8338449d1b3706274cc2e02321254038188fce91
mateusz <91789211+Mat3u52@users.noreply.github.com>
2022-08-24 06:48:38 +0200

    update

--- ContextualMenu.py
+++ ContextualMenu.py
@@ -1,5 +1,5 @@
-from tkinter import ttk
 from tkinter import *
+import tkinter as tk
 import pyperclip
 
 class ContextualMenu:
@@ -8,23 +8,23 @@ class ContextualMenu:
         self.captureEntry = StringVar()
 
         self.contextMenu = Menu(self.root, tearoff=0)
-        self.contextMenu.add_command(label="Copy", command=self.copy)
-        #self.contextInsertMenu.add_command(label="Copy", command=pyperclip.copy(self.test))
-        #self.contextInsertMenu.add_command(label="Paste", command=contextPasteEI2)
-
-
+        self.contextMenu.add_command(label="Copy", command=self._copy)
+        self.contextMenu.add_command(label="Paste", command=self._paste)
 
     def doPopup(self, event):
         try:
             self.contextMenu.tk_popup(event.x_root, event.y_root)
-            #print(self.text)
         finally:
             self.contextMenu.grab_release()
 
-    def copy(self):
-        pyperclip.copy(self.captureEntry.get())
-        #print(self.text.get())
+    def _copy(self):
+        try:
+            pyperclip.copy(self.captureEntry.get())
+        except AttributeError:
+            pass
 
+    def setEntry(self, entryFild):
+        self.entryFild = entryFild
 
-    #def contextPasteEI2(self):
-    #    EI2.insert(tk.END, pyperclip.paste())
+    def _paste(self):
+        self.entryFild.insert(tk.END, pyperclip.paste())
