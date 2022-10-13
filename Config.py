73a95bef899831f2f76a16f0b3f720d9f2970c35
mateusz <91789211+Mat3u52@users.noreply.github.com>
2022-10-13 04:38:10 +0200

    update

---
+++ Config.py
@@ -0,0 +1,31 @@
+class Config:
+    def __init__(self):
+        self.title = 'AXI - Manager'
+        self.screenWidth = 690
+        self.screenHeight = 820
+        self.bgColor = '#000000'
+
+        self.scrollX = 625
+        self.scrollY = 27
+        self.scrollHeight = 423
+
+        self.pathImgDefault = 'img/lackOfPicture/board.png'
+
+        self.pathImg5DX1 = '5DX/images/V849/'
+        #self.pathImg5DX1 = 'Y:/5DX/images/V849/'
+
+        self.pathImg5DX2 = '5DX/images/V817/'
+        #self.pathImg5DX2 = 'Y:/5DX/images/V817/'
+
+        self.pathImgV8103163 = 'images/V810-3163/'
+        #self.pathImgV8103163 = 'X:/images/V810-3163/'
+
+        self.pathImgV8103483S2EX = 'images/V810-3483S2EX/'
+        #self.pathImgV8103483S2EX = 'X:/images/V810-3483S2EX/'
+
+        self.pathImgV8103553S2EX = 'images/V810-3553S2EX/'
+        #self.pathImgV8103553S2EX = 'X:/images/V810-3553S2EX/'
+
+        self.pathImgV8108120S2 = 'images/V810-8120S2/'
+        #self.pathImgV8108120S2 = 'X:/images/V810-8120S2/'
+
