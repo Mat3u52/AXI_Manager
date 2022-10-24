8063992ba61bb104a0b747669b8673e53b395e04
mateusz <91789211+Mat3u52@users.noreply.github.com>
2022-10-24 04:54:40 +0200

    update

--- Styles.py
+++ Styles.py
@@ -12,6 +12,7 @@ class Styles:
                                        'borderwidth': '10'
                                    }
                                },
+
                                'TLabelframe.Label': {
                                    'configure': {
                                        'background': '#000000',
@@ -21,12 +22,29 @@ class Styles:
 
                                'TButton': {
                                    'configure': {
-                                       'background': '#111111',  # 302928
+                                       'background': '#111111',
                                        'foreground': '#FFFFFF',
                                        'anchor': 'N',
                                        'font': ("Arial", 12, 'bold'),
                                        'borderwidth': 1,
                                        'relief': "solid"
+                                   }
+                               },
+
+                               'AutomaticInsert.TRadiobutton': {
+                                   'configure': {
+                                       'indicatorrelief': 'tk.FLAT',
+                                       'indicatormargin': '-1',
+                                       'indicatordiameter': '-1',
+                                        'relief': 'tk.RAISED',
+                                        'focusthickness': '8',
+                                        'highlightthickness': '#000000',
+                                        'padding': '4',
+                                        'background': '#444444',
+                                        'foreground': '#AAAAAA'
+                                   },
+                                   'map': {
+                                       'background': [('selected', '#444444'), ('active', '#777777')]
                                    }
                                }
                            }
@@ -39,9 +57,11 @@ class Styles:
         self.noteStyler.configure("TNotebook.Tab", background='#555555', foreground='#FFFFFF', lightcolor='#FFFFFF',
                              borderwidth=1)
         self.noteStyler.configure("TFrame", background='#444444', foreground='#FFFFFF', borderwidth=1)
+
         self.style.configure("TCombobox", fieldbackground="#333333", background="#302928", borderwidth=0)
         self.root.option_add("*TCombobox*Listbox*Background", "#302928")
         self.root.option_add("*TCombobox*Listbox*Foreground", "#AAAAAA")
 
         self.style.configure("Treeview", background="#000000", foreground="#FFFFFF", rowheight=40, filedbackground="#777777")
         self.style.map('Treeview', background=[('selected', '#46464A')])
+
