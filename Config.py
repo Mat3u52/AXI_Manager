30acb9eda8c98f6a47a7d63258b427b30813d651
mateusz <91789211+Mat3u52@users.noreply.github.com>
2022-11-06 05:11:12 +0100

    update

--- Config.py
+++ Config.py
@@ -12,25 +12,26 @@ class Config:
 
         self.pathImgDefault = 'img/lackOfPicture/board.png'
 
-        self.pathLog = 'C:\\_PythonProject\\AXI_Manager\\Log'
-        self.pathRecipe = 'C:\\_PythonProject\\AXI_Manager\\images'
+        self.pathLog = 'C:\\_PythonProject\\AXI_Manager_Source_Files\\Log'
+        self.pathRecipe = 'C:\\_PythonProject\\AXI_Manager_Source_Files\\images'
         self.devices = ('V810-3163', 'V810-3483S2EX', 'V810-3553S2EX', 'V810-8120S2')
 
-        self.pathImg5DX1 = '5DX/images/V849/'
+        self.pathImg5DX1 = 'C:/_PythonProject/AXI_Manager_Source_Files/5DX/images/V849/'
         #self.pathImg5DX1 = 'Y:/5DX/images/V849/'
 
-        self.pathImg5DX2 = '5DX/images/V817/'
+        self.pathImg5DX2 = 'C:/_PythonProject/AXI_Manager_Source_Files/5DX/images/V817/'
+        #self.pathImg5DX2 = '5DX/images/V817/'
         #self.pathImg5DX2 = 'Y:/5DX/images/V817/'
 
-        self.pathImgV8103163 = 'images/V810-3163/'
+        self.pathImgV8103163 = 'C:/_PythonProject/AXI_Manager_Source_Files/images/V810-3163/'
         #self.pathImgV8103163 = 'X:/images/V810-3163/'
 
-        self.pathImgV8103483S2EX = 'images/V810-3483S2EX/'
+        self.pathImgV8103483S2EX = 'C:/_PythonProject/AXI_Manager_Source_Files/images/V810-3483S2EX/'
         #self.pathImgV8103483S2EX = 'X:/images/V810-3483S2EX/'
 
-        self.pathImgV8103553S2EX = 'images/V810-3553S2EX/'
+        self.pathImgV8103553S2EX = 'C:/_PythonProject/AXI_Manager_Source_Files/images/V810-3553S2EX/'
         #self.pathImgV8103553S2EX = 'X:/images/V810-3553S2EX/'
 
-        self.pathImgV8108120S2 = 'images/V810-8120S2/'
+        self.pathImgV8108120S2 = 'C:/_PythonProject/AXI_Manager_Source_Files/images/V810-8120S2/'
         #self.pathImgV8108120S2 = 'X:/images/V810-8120S2/'
 
