a0c216fffc9ab155bc751a217de958e6a9b8b67f
Mat3u52 <kolodziej2007@tlen.pl>
2023-02-09 05:57:00 +0100

    comparison two 'set'

--- Comparison.py
+++ Comparison.py
@@ -1,6 +1,7 @@
 import os
 from DBConnect import DBConnect
 
+
 class Comparison:
     def __init__(self, dir_name: str) -> None:
         self.dir_name: str = dir_name
@@ -8,7 +9,7 @@ class Comparison:
     def __repr__(self):
         return self.dir_name
 
-    #@staticmethod
+    # @staticmethod
     def comparison_error(self) -> bool:
         if os.path.exists(self.dir_name):
             return True
@@ -26,17 +27,17 @@ class Comparison:
 
 if __name__ == "__main__":
     obj_comparison = Comparison("/root/PythonDeveloper/AXI_Manager_Source_Files/images/V810-3163/")
-    print(obj_comparison)
     if obj_comparison.comparison_error():
-        #recipes_list = os.listdir(obj_comparison.dir_name)
-        print(obj_comparison.recipes_list())
+        # print(obj_comparison.recipes_list())
         obj_DBConnect = DBConnect()
-        print(obj_DBConnect.select_recipe('VITROXI_PROG'))
 
         inventory_db: set[str] = set()
         for prog_db in obj_DBConnect.select_recipe('VITROXI_PROG'):
-            inventory_db.add(prog_db)
-        print(inventory_db)
+            inventory_db.add(prog_db[0])
+        # print(inventory_db)
 
+        # png to db
         print(obj_comparison.recipes_list().difference(inventory_db))
+        # db to png
+        print(inventory_db.difference(obj_comparison.recipes_list()))
 
