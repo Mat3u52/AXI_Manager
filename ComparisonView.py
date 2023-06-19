5b83c76ea3b71b0dad86e0f92084ea877f4c40b6
Mat3u52 <kolodziej2007@tlen.pl>
2023-06-19 07:07:24 +0200

    class for comparison tab

---
+++ ComparisonView.py
@@ -0,0 +1,9 @@
+from tkinter import Label, W, END, E, IntVar, Entry, ttk, messagebox, PhotoImage, Button, StringVar
+
+
+class ComparisonView:
+    def __init__(self) -> None:
+        """
+        Constructor of ComparisonView class. Preparation of a few properties.
+        """
+
