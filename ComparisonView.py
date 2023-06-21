30792b4c899d44cbe89afab98969350b5e60e8ee
Mat3u52 <kolodziej2007@tlen.pl>
2023-06-21 07:05:44 +0200

    pyperclip

--- ComparisonView.py
+++ ComparisonView.py
@@ -1,9 +1,16 @@
 from tkinter import Label, W, END, E, IntVar, Entry, ttk, messagebox, PhotoImage, Button, StringVar
+import tkinter as tk
 
 
 class ComparisonView:
-    def __init__(self) -> None:
+    def __init__(self, root: tk) -> None:
         """
         Constructor of ComparisonView class. Preparation of a few properties.
+
+        :return: Init class
+        :rtype: None
         """
+        self.root = root
+
+
 
