cb57afac7734164df9277652376c9fde91f58a53
Mat3u52 <kolodziej2007@tlen.pl>
2023-06-27 06:33:19 +0200

    Comparison tab is modify to oop

--- ComparisonView.py
+++ ComparisonView.py
@@ -1,14 +1,10 @@
 from tkinter import Label, W, END, E, IntVar, Entry, ttk, messagebox, PhotoImage, Button, StringVar
 import tkinter as tk
 from Comparison import Comparison
+from Styles import Styles
 
 
 class ComparisonView(Comparison):
-
-    # v = StringVar(root, "0")
-    # v0 = StringVar(root, "0")
-    # extract_set: dict = {}
-    # extract_set0: dict = {}
 
     def __init__(self, dir_name: str, db_name: str, root: tk) -> None:
         """
@@ -21,31 +17,35 @@ class ComparisonView(Comparison):
         """
         super().__init__(dir_name, db_name)
 
+        self.v = StringVar(root, "0")
+        self.v0 = StringVar(root, "0")
+        self.extract_set: dict = {}
+        self.extract_set0: dict = {}
+
         self.root = root
 
-    def compare(self):
-        pass
-        # obj_comparison = Comparison(dir_name="/root/PythonDeveloper/AXI_Manager_Source_Files/images/V810-3163/",
-        #                             db_name="VITROXI_PROG")
-        # i: int = 0
-        # extract_set0.clear()
-        # for value in dict.fromkeys(obj_comparison.recipes_list().difference(obj_comparison.recipes_db()), 0):
-        #     extract_set0[i] = value
-        #     i += 1
-        #
-        # row_count: int = 0
-        # for (text, value) in extract_set0.items():
-        #     if len(value) > 0:
-        #         ttk.Radiobutton(tab_3163_to_db,
-        #                         text=value,
-        #                         variable=v0,
-        #                         value=text,
-        #                         style="Comparison.TRadiobutton",
-        #                         command=insert_from_comparison0,
-        #                         ).grid(row=int(row_count),
-        #                                column=0,
-        #                                sticky=W)
-        #         row_count += 1
+    def compare_list_to_db(self):
+        print(self.recipes_list().difference(self.recipes_db()))
+
+        i: int = 0
+        self.extract_set0.clear()
+        for value in dict.fromkeys(self.recipes_list().difference(self.recipes_db()), 0):
+            self.extract_set0[i] = value
+            i += 1
+
+        row_count: int = 0
+        for (text, value) in self.extract_set0.items():
+            if len(value) > 0:
+                ttk.Radiobutton(tab_3163_to_db,
+                                text=value,
+                                variable=self.v0,
+                                value=text,
+                                style="Comparison.TRadiobutton",
+                                command=insert_from_comparison0,
+                                ).grid(row=int(row_count),
+                                       column=0,
+                                       sticky=W)
+                row_count += 1
 
 
 
