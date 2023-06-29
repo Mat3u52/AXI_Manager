3c715124a93a599bdc9c89308cabcdded1f76db1
Mat3u52 <kolodziej2007@tlen.pl>
2023-06-29 06:44:28 +0200

    Comparison tab is modify to oop

--- ComparisonView.py
+++ ComparisonView.py
@@ -6,7 +6,7 @@ from Styles import Styles
 
 class ComparisonView(Comparison):
 
-    def __init__(self, dir_name: str, db_name: str, root: tk) -> None:
+    def __init__(self, dir_name: str, db_name: str, root: tk, tab: ttk.Frame) -> None:
         """
         Constructor of ComparisonView class. Preparation of a few properties.
 
@@ -17,35 +17,46 @@ class ComparisonView(Comparison):
         """
         super().__init__(dir_name, db_name)
 
-        self.v = StringVar(root, "0")
-        self.v0 = StringVar(root, "0")
-        self.extract_set: dict = {}
-        self.extract_set0: dict = {}
-
         self.root = root
+        self.tab = tab
+
+        # self.v = StringVar(root, "0")
+        self.var = StringVar(self.root, "0")
+        # self.extract_set: dict = {}
+        self.extract_set_to_dic: dict = {}
 
     def compare_list_to_db(self):
-        print(self.recipes_list().difference(self.recipes_db()))
+        # print(self.recipes_list().difference(self.recipes_db()))
 
         i: int = 0
-        self.extract_set0.clear()
+        self.extract_set_to_dic.clear()
         for value in dict.fromkeys(self.recipes_list().difference(self.recipes_db()), 0):
-            self.extract_set0[i] = value
+            self.extract_set_to_dic[i] = value
             i += 1
-
+        print(self.extract_set_to_dic)
         row_count: int = 0
-        for (text, value) in self.extract_set0.items():
+        for (text, value) in self.extract_set_to_dic.items():
             if len(value) > 0:
-                ttk.Radiobutton(tab_3163_to_db,
+                ttk.Radiobutton(self.tab,
                                 text=value,
-                                variable=self.v0,
+                                variable=self.var,
                                 value=text,
                                 style="Comparison.TRadiobutton",
-                                command=insert_from_comparison0,
+                                command=self._insert_from_comparison,
                                 ).grid(row=int(row_count),
                                        column=0,
                                        sticky=W)
                 row_count += 1
+
+    def _insert_from_comparison(self) -> None:
+        """
+        Put data to clipboard after click on name
+        :return: None
+        :rtype: None
+        """
+        pass
+        # print(extract_set[int(v.get())])
+        # pyperclip.copy(extract_set[int(v.get())])
 
 
 
