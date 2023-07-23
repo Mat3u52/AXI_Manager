94cbc452646fd4e42c36348a288d0c3b7ef640ab
Mat3u52 <kolodziej2007@tlen.pl>
2023-07-23 05:58:13 +0200

    Remove record

--- RemoveRecord.py
+++ RemoveRecord.py
@@ -1,4 +1,5 @@
 import os
+import re
 from DBConnect import DBConnect
 from Config import Config
 
@@ -18,12 +19,37 @@ class RemoveRecord(DBConnect):
             # print(one_record[0][1])
 
             dirs_and_files = os.listdir(Config().pathRecipe)
+
             for device in dirs_and_files:
-                container: set[str] = set()
+                # container: set[str] = set()
                 print(os.path.join(Config().pathRecipe, device))
-                container = set(os.listdir(os.path.join(Config().pathRecipe, device)))
-                print(container)
+                container: set[str] = set(os.listdir(os.path.join(Config().pathRecipe, device)))
 
+                # txt = "__test03.txt"
+                # search_for = re.search(f"^{self.selectSearchID(self.id_record)[0][1]}....$", txt)
+                # if search_for:
+                #     print("yes")
+                # else:
+                #     print("no")
+                test1 = "test"
+                test = re.compile(r".(?:txt|jpeg|jpg|png)")
+                print(type(test))
+                str2 = "test.txt"
+                result = test.findall(str2)
+                print(result)
+                if test1 + test is str2:
+                    print('ok')
+                # if test in container:
+                #     print('exist')
+                # else:
+                #     print('not exist')
+                # print(container)
 
-        except:
+                # if self.selectSearchID(self.id_record)[0][1] in container:
+                #     print('exist')
+                # else:
+                #     print('not exist')
+                # print(container)
+
+        except ValueError:
             print("Record do not exist.")
