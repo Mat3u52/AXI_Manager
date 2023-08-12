509694db88e8444a79cb58806e8c84003170a949
Mat3u52 <kolodziej2007@tlen.pl>
2023-08-12 19:45:18 +0200

    Remove record

--- AXI_Manager.py
+++ AXI_Manager.py
@@ -2010,12 +2010,17 @@ if __name__ == "__main__":
     add_frame = ttk.LabelFrame(tab3, text=" New Items: ")
 
 # --- Comparison ---
-    v810_3163 = ttk.Notebook(tab4)
-    tab_3163 = ttk.Frame(v810_3163)
-    v810_3163.add(tab_3163, text=" --- V810-3163 --- ")
-    v810_3163.pack(expand=1, fill="both", padx=10, pady=10)
+    v810_device = ttk.Notebook(tab4)
+    tab_3163 = ttk.Frame(v810_device)
+    v810_device.add(tab_3163, text=" --- V810-3163 --- ")
+    v810_device.pack(expand=1, fill="both", padx=10, pady=10)
 
-    db_to_v810 = ttk.Notebook(v810_3163)
+    # v810_3483S2EX = ttk.Notebook(tab4)
+    tab_3483S2EX = ttk.Frame(v810_device)
+    v810_device.add(tab_3483S2EX, text=" --- V810-3483S2EX --- ")
+    v810_device.pack(expand=1, fill="both", padx=10, pady=10)
+
+    db_to_v810 = ttk.Notebook(v810_device)
 
     obj_comparison = ComparisonView(
         dir_name=obj_config.pathImgV8103163,
@@ -2032,26 +2037,21 @@ if __name__ == "__main__":
     db_to_v810.pack(expand=1, fill="both", padx=25, pady=25)
     obj_comparison.compare_db_to_list(tab=tab_db_to_3163)
 
-
-    v810_3483S2EX = ttk.Notebook(tab4)
-    tab_3483S2EX = ttk.Frame(v810_3483S2EX)
-    v810_3483S2EX.add(tab_3483S2EX, text=" --- V810-3483S2EX --- ")
-    v810_3483S2EX.pack(expand=1, fill="both", padx=10, pady=10)
-
-    db_to_v8102 = ttk.Notebook(v810_3483S2EX)
+    db_to_v8102 = ttk.Notebook(v810_device)
 
     obj_comparison = ComparisonView(
         dir_name=obj_config.pathImgV8103483S2EX,
         db_name="VITROXIII_PROG",
         root=root
     )
+
     tab_3483S2EX_to_db = ttk.Frame(db_to_v8102)
-    db_to_v8102.add(tab_3483S2EX_to_db, text=" --- V810 to DB --- ")
+    db_to_v8102.add(tab_3483S2EX_to_db, text=" --- V8100 to DB --- ")
     db_to_v8102.pack(expand=1, fill="both", padx=25, pady=25)
     obj_comparison.compare_list_to_db(tab=tab_3483S2EX_to_db)
 
     tab_db_to_3483S2EX = ttk.Frame(db_to_v8102)
-    db_to_v8102.add(tab_db_to_3483S2EX, text=" --- DB to V810 --- ")
+    db_to_v8102.add(tab_db_to_3483S2EX, text=" --- DB to V8100 --- ")
     db_to_v8102.pack(expand=1, fill="both", padx=25, pady=25)
     obj_comparison.compare_db_to_list(tab=tab_db_to_3483S2EX)
 
