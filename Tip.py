7f39c62772f509017e81fe758711ddbb91dbad1b
Mat3u52 <kolodziej2007@tlen.pl>
2023-09-04 05:51:23 +0200

    Refactoring

--- Tip.py
+++ Tip.py
@@ -6,62 +6,67 @@ from PIL import Image
 
 class Tip:
 
-    def __init__(self, root: tk, mainFrameView: tk, primePng: str = 'img/tip/okBegin.png') -> None:
+    def __init__(self, root: tk, main_frame_view: tk, prime_png: str = 'img/tip/okBegin.png') -> None:
         """
         Preparation of canvas area.
 
+        :param root: handel to main window
+        :type root: tk
+        :param main_frame_view: handel to frame
+        :type main_frame_view: tk
+        :param prime_png: path to the .png file
+        :type prime_png: str
         :return: prepare canvas area
         :rtype: None
         """
 
         self.root = root
-        self.mainFrameView = mainFrameView
-        self.startXPosition = 120
-        self.startYPosition = 20
-        self.minMovement = -1
-        self.refreshSec = 0.01
-        self.primePng = primePng
+        self.main_frame_view = main_frame_view
+        self.start_x_position = 120
+        self.start_y_position = 20
+        self.min_movement = -1
+        self.refresh_sec = 0.01
+        self.prime_png = prime_png
 
-        self.canvas_frame = Label(self.mainFrameView)
+        self.canvas_frame = Label(self.main_frame_view)
         self.canvas_frame.configure(bg="#333333", bd=0)
         self.canvas_frame.grid(row=0, column=7, sticky=W)
-        self.canv = tk.Canvas(self.canvas_frame, width=120, height=30)
-        self.canv.configure(bg="#333333", bd=0, highlightthickness=0, highlightbackground="black")
-        self.canv.pack(expand=False)
+        self.canvas_created = tk.Canvas(self.canvas_frame, width=120, height=30)
+        self.canvas_created.configure(bg="#333333", bd=0, highlightthickness=0, highlightbackground="black")
+        self.canvas_created.pack(expand=False)
 
     def animate_translucent(self) -> None:
+        """
+        Translucent of image
 
-        self.canvasFrame1 = Label(self.mainFrameView)
-        self.canvasFrame1.configure(bg="#333333", bd=0)
-        self.canvasFrame1.grid(row=0, column=7, sticky=W)
-        self.canvas1 = tk.Canvas(self.canvasFrame1, width=120, height=30)
-        self.canvas1.configure(bg="#333333", bd=0, highlightthickness=0, highlightbackground="black")
-        self.canvas1.pack(expand=False)
+        :return: transparent
+        :rtype: None
+        """
 
-        self.im = Image.open(self.primePng)
-        self.im.putalpha(120)
-        self.im.save('img/tip/ok.png')
-        self.photo = PhotoImage(file='img/tip/ok.png')
-        self.imageB = self.canvas1.create_image(120, 15, image=self.photo)
+        im = Image.open(self.prime_png)
+        im.putalpha(120)
+        im.save('img/tip/ok.png')
+        photo = PhotoImage(file='img/tip/ok.png')
+        image_b = self.canvas_created.create_image(120, 15, image=photo)
 
         while True:
-            self.canvas1.move(self.imageB, self.minMovement, 0)
+            self.canvas_created.move(image_b, self.min_movement, 0)
             self.root.update()
-            time.sleep(self.refreshSec)
-            self.imgPos = self.canvas1.coords(self.imageB)
+            time.sleep(self.refresh_sec)
+            img_pos = self.canvas_created.coords(image_b)
 
-            self.al, self.bl = self.imgPos
+            al, bl = img_pos
 
-            self.transparency = 120 + (2*(int(self.al) - 120))
-            self.im = Image.open('img/tip/ok.png')
-            self.im.putalpha(self.transparency)
-            self.im.save('img/tip/ok.png')
-            self.photo = PhotoImage(file='img/tip/ok.png')
-            self.imageB = self.canvas1.create_image(self.al, self.bl, image=self.photo)
+            transparency = 120 + (2*(int(al) - 120))
+            im = Image.open('img/tip/ok.png')
+            im.putalpha(transparency)
+            im.save('img/tip/ok.png')
+            photo = PhotoImage(file='img/tip/ok.png')
+            image_b = self.canvas_created.create_image(al, bl, image=photo)
 
-            if self.al < abs(self.minMovement):
-                self.minMovement = -self.minMovement
-            if self.al == int(self.startXPosition) / 2:
+            if al < abs(self.min_movement):
+                self.min_movement = -self.min_movement
+            if al == int(self.start_x_position) / 2:
                 break
 
     def animate_non_translucent(self) -> None:
@@ -72,23 +77,21 @@ class Tip:
         :rtype: None
         """
 
-        photo = PhotoImage(file='img/tip/okBegin.PNG')
-        imageB = self.canv.create_image(120, 15, image=photo)
+        photo = PhotoImage(file='img/tip/okBegin.png')
+        image_b = self.canvas_created.create_image(120, 15, image=photo)
 
         while True:
-            self.canv.move(imageB, self.minMovement, 0)
+            self.canvas_created.move(image_b, self.min_movement, 0)
             self.root.update()
-            time.sleep(self.refreshSec)
-            imgPos = self.canv.coords(imageB)
+            time.sleep(self.refresh_sec)
+            img_pos = self.canvas_created.coords(image_b)
+            al, bl = img_pos
+            photo = PhotoImage(file='img/tip/okBegin.png')
+            image_b = self.canvas_created.create_image(al, bl, image=photo)
 
-            al, bl = imgPos
-
-            photo = PhotoImage(file='img/tip/okBegin.PNG')
-            imageB = self.canv.create_image(al, bl, image=photo)
-
-            if al < abs(self.minMovement):
-                self.minMovement = -self.minMovement
-            if al == int(self.startXPosition) / 2:
+            if al < abs(self.min_movement):
+                self.min_movement = -self.min_movement
+            if al == int(self.start_x_position) / 2:
                 break
 
     def __del__(self) -> None:
