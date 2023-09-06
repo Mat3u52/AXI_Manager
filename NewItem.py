5916f2e6c9e532d3a3de0f81ae4caa963f3d122d
Mat3u52 <kolodziej2007@tlen.pl>
2023-09-06 05:49:39 +0200

    Refactoring

--- NewItem.py
+++ NewItem.py
@@ -16,30 +16,31 @@ class NewItem:
         :rtype: None
         """
 
-        self.tab = tab
-        self.root = root
+        self.tab: ttk = tab
+        self.root: ttk = root
+        self.main_frame: ttk = None
 
-    def mainFrameInsert(self, main_name):
-        self.mainName = main_name
-        self.mainFrameInsert = ttk.LabelFrame(self.tab, text=self.mainName)
-        self.mainFrameInsert.grid(column=0, row=2, columnspan=10, sticky='W', padx=10, pady=10)
-        self.LI2 = Label(self.mainFrameInsert, text="Item:", width=12, borderwidth=1,
+    def main_frame_insert(self, main_name: str = " ") -> None:
+        main_name: str = main_name
+        self.main_frame = ttk.LabelFrame(self.tab, text=main_name)
+        self.main_frame.grid(column=0, row=2, columnspan=10, sticky='W', padx=10, pady=10)
+        self.LI2 = Label(self.main_frame, text="Item:", width=12, borderwidth=1,
                          relief="solid", bg="#212121", fg="#555555", pady="1")
         self.LI2.config(font=("Arial", 10))
-        self.LI3 = Label(self.mainFrameInsert, text="Qty:", width=12, borderwidth=1,
+        self.LI3 = Label(self.main_frame, text="Qty:", width=12, borderwidth=1,
                          relief="solid", bg="#212121", fg="#555555", pady="1")
         self.LI3.config(font=("Arial", 10))
         self.LI2.grid(row=0, column=0, sticky=W)
         self.LI3.grid(row=0, column=2, sticky=W)
         self.objCM = ContextualMenu(self.root, '0', True, True, False)
-        self.EI2 = Entry(self.mainFrameInsert, relief="solid", textvariable=self.objCM.captureEntry,
+        self.EI2 = Entry(self.main_frame, relief="solid", textvariable=self.objCM.captureEntry,
                          borderwidth=1, width=35, bg="#212121", fg="#FFFFFF")
         self.EI2.config(font=("Arial", 10), highlightbackground="#000000", highlightcolor="#33FFBE")
         self.EI2.grid(row=0, column=1, pady=1)
         self.EI2.bind("<Button-3>", self.objCM.do_popup)
         self.objCM.set_entry(self.EI2)
 
-        self.EI3 = Entry(self.mainFrameInsert, relief="solid", borderwidth=1, width=10, bg="#212121", fg="#FFFFFF")
+        self.EI3 = Entry(self.main_frame, relief="solid", borderwidth=1, width=10, bg="#212121", fg="#FFFFFF")
         self.EI3.config(font=("Arial", 10), highlightbackground="#000000", highlightcolor="#33FFBE")
         self.EI3.grid(row=0, column=3, pady=1, stick=W)
 
