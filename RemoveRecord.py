fff68516a787013595c17677edcfe348ed4fce0e
Mat3u52 <kolodziej2007@tlen.pl>
2023-09-28 06:17:25 +0200

    Refactoring

--- RemoveRecord.py
+++ RemoveRecord.py
@@ -9,10 +9,26 @@ class RemoveRecord(DBConnect):
     flag: bool = False
 
     def __init__(self, id_record: str) -> None:
+        """
+        Preparation to remove the record.
+
+        :param id_record: identity number of the record
+        :type id_record: str
+        :return: Initialization to remove
+        :rtype: None
+        """
+
         super().__init__()
         self.id_record: str = str(id_record)
 
     def remove_total(self) -> bool:
+        """
+        The method remove all data associate with id.
+
+        :return: remove all data
+        :rtype: None
+        """
+
         msg_box = tk.messagebox.askquestion('Remove Record',
                                             f'Are you sure you want to remove'
                                             f' {self.selectSearchID(self.id_record)[0][1]} '
@@ -20,9 +36,6 @@ class RemoveRecord(DBConnect):
                                             icon='warning')
         if msg_box == 'yes':
             try:
-                # print("removed")
-                # print(self.selectSearchID(self.id_record)[0][1])
-
                 dirs_and_files = os.listdir(Config().pathRecipe)
 
                 recipes: tuple = (self.selectSearchID(self.id_record)[0][17],
@@ -31,36 +44,29 @@ class RemoveRecord(DBConnect):
                                   self.selectSearchID(self.id_record)[0][31],
                                   self.selectSearchID(self.id_record)[0][45],
                                   self.selectSearchID(self.id_record)[0][54])
-                print(recipes)
                 for device in dirs_and_files:
-                    # print(os.path.join(Config().pathRecipe, device))
                     container: set[str] = set(os.listdir(os.path.join(Config().pathRecipe, device)))
-
                     for recipe in recipes:
                         if str(recipe) + r".png" in container or \
                                 str(recipe) + r".jpg" in container or \
                                 str(recipe) + r".txt" in container:
-                            print(str(recipe))
                             path = os.path.join(Config().pathRecipe, device)
                             try:  # .png
-                                print(f"{os.path.join(path, str(recipe))}.png")
-                                # os.remove(f"{os.path.join(path, self.selectSearchID(self.id_record)[0][1])}.png")
+                                os.remove(f"{os.path.join(path, self.selectSearchID(self.id_record)[0][1])}.png")
                             except FileNotFoundError:
                                 print("The .png file does not exist!")
                             try:  # .jpg
-                                print(f"{os.path.join(path, str(recipe))}.jpg")
-                                # os.remove(f"{os.path.join(path, self.selectSearchID(self.id_record)[0][1])}.jpg")
+                                os.remove(f"{os.path.join(path, self.selectSearchID(self.id_record)[0][1])}.jpg")
                             except FileNotFoundError:
                                 print("The .jpg file does not exist!")
                             try:  # .txt
-                                print(f"{os.path.join(path, str(recipe))}.txt")
-                                # os.remove(f"{os.path.join(path, self.selectSearchID(self.id_record)[0][1])}.txt")
+                                os.remove(f"{os.path.join(path, self.selectSearchID(self.id_record)[0][1])}.txt")
                             except FileNotFoundError:
                                 print("The .txt file does not exist!")
                         else:
                             print('The .png or .jpg or .txt file does not exist!')
 
-                # self.delete_by_id(self.id_record)
+                self.delete_by_id(self.id_record)
                 RemoveRecord.flag = True
                 return True
             except ValueError:
