b845c590e6c9b11196cb7de31f104243eaea320c
Mat3u52 <kolodziej2007@tlen.pl>
2023-10-08 06:06:04 +0200

    Refactoring

--- Animation.py
+++ Animation.py
@@ -1,16 +1,14 @@
 import tkinter
 import time
 import os
+from Config import Config
 
 
 class Animation:
-    min_movement: int = -1
 
     def __init__(self,
                  ai_root: tkinter.Tk,
                  canvas: tkinter.Canvas,
-                 x_pos: int = -1,
-                 y_pos: int = -1,
                  img_path: str = "board.png",
                  ) -> None:
         """
@@ -20,24 +18,17 @@ class Animation:
         :type ai_root: tkinter.Tk
         :param canvas: Given object from tkinter.Canvas
         :type canvas: tkinter.Canvas
-        :param x_pos: Given value of axis x
-        :type x_pos: int
-        :param y_pos: Given value of axis y
-        :type y_pos: int
         :param img_path: Given path to .png file
         :type img_path: str
         :return: Initialize the variables
         :rtype: None
         """
+
         self.ai_root: tkinter.Tk = ai_root
         self.canvas: tkinter.Canvas = canvas
-        self.x_pos: int = x_pos
-        self.y_pos: int = y_pos
+        self.x_pos: int = -1
+        self.y_pos: int = -1
         self.img_path: str = img_path
-
-        self.start_x_position: int = 170
-        self.start_y_position: int = 85
-        self.refresh_sec: float = 0.01
 
     def move_image(self) -> None:
         """
@@ -46,25 +37,26 @@ class Animation:
         :return: animate the picture
         :rtype: None
         """
-        # start_x_position: int = 170
-        # start_y_position: int = 85
-        # refresh_sec: float = 0.01
+        start_x_position: int = 170
+        start_y_position: int = 85
+        refresh_sec: float = 0.01
         if os.path.isfile(self.img_path):
             img = tkinter.PhotoImage(file=self.img_path)
         else:
-            img = tkinter.PhotoImage(file="img/lackOfPicture/board.png")
+            # img = tkinter.PhotoImage(file="img/lackOfPicture/board.png")
+            img = tkinter.PhotoImage(Config().pathImgDefault)
 
-        ai_image = self.canvas.create_image(self.start_x_position, self.start_y_position, image=img)
+        ai_image = self.canvas.create_image(start_x_position, start_y_position, image=img)
 
         while True:
-            self.canvas.move(ai_image, x_pos, 0)
+            self.canvas.move(ai_image, self.x_pos, 0)
             self.ai_root.update()
-            time.sleep(self.refresh_sec)
+            time.sleep(refresh_sec)
             img_pos = self.canvas.coords(ai_image)
             al, bl = img_pos
-            if al < abs(x_pos):
-                x_pos = -x_pos
-            if bl < abs(y_pos):
-                y_pos = -y_pos
-            if al == int(self.start_x_position) / 2:
+            if al < abs(self.x_pos):
+                self.x_pos = -self.x_pos
+            if bl < abs(self.y_pos):
+                self.y_pos = -self.y_pos
+            if al == int(start_x_position) / 2:
                 break
