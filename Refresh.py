6f6b869e7effcef2c74494addce9fcc98c75ebdd
Mat3u52 <kolodziej2007@tlen.pl>
2023-10-28 05:50:45 +0200

    Refactoring

--- Refresh.py
+++ Refresh.py
@@ -5,14 +5,31 @@ class Refresh:
     def __init__(self) -> None:
         pass
 
-    def baan_status(self, *args) -> None:
-        status = None
-        for baan_status in args:
-            if baan_status == 'NO':
-                status = 0
+    def foreign_app_status(self, *args) -> bool:
+        """
+        The method is verifying the status of "_BAAN1, _LINECAPA, _EPI" in the DB.
+        example: `5DX_BAAN1` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL
+
+        :return: True - if status is no or lack and False otherwise
+        :rtype: bool
+        """
+        status: bool = False
+        container_ineligible: tuple = ("YES", "Yes", "yes", "NONE", "None", "none")
+        container_eligible: tuple = ("NO", "No", "no", "LACK", "Lack", "lack")
+        for app_status in args:
+            if app_status not in container_ineligible \
+                    and app_status is not None \
+                    and app_status in container_eligible:
+                status = True
         return status
 
 
 if __name__ == "__main__":
     obj_refresh = Refresh
-    print(obj_refresh.baan_status('None', 'yes', 'NO'))
+    print(obj_refresh.foreign_app_status('None', 'yes', '', None))
+
+
+# row[11] != "YES"
+# and row[11] != "NONE"
+# and row[11] is not None
+# and (row[11] == "NO" or row[11] == "LACK")
